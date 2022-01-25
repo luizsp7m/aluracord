07b3e6329c4349494b299f3ef354affdf6473e16
Luiz Oliveira <luizoliveira2808@gmail.com>
2022-01-25 19:34:56 -0300

    Favicon adicionado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Montserrat:wght@700&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/assets/favicon.png" type="image/x-icon" />
         </Head>
 
         <body>
