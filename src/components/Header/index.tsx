247b9c44b26ac6e001405c21e9c58ef60fd81477
Luiz Oliveira <luizoliveira2808@gmail.com>
2022-08-05 01:26:17 -0300

    style: Mudanças no botão sign out

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import styles from "./styles.module.scss";
 
 import { useRouter } from "next/router";
 import { destroyCookie } from "nookies";
+import { AiFillGithub } from "react-icons/ai";
 
 interface HeaderProps {
   user_session: string;
@@ -19,10 +20,16 @@ export function Header({ user_session }: HeaderProps) {
     <div className={styles.header}>
       <img src="/assets/favicon.png" alt="Lobo" />
 
-      <div>
+      {/* <div>
         <img src={`https://github.com/${user_session}.png`} alt="" />
         <span onClick={logout}>Sair</span>
-      </div>
+      </div> */}
+
+      <button type="button" className={styles.signOutButton} onClick={logout}>
+        <AiFillGithub size={22} />
+        <h5>{user_session}</h5>
+        <span>&times;</span>
+      </button>
     </div>
   );
 }
